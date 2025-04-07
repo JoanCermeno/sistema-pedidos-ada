4515cc66ef3084fa9175cda7c93b264d1294b6fa
Joan Cermeno <joangabriel684@gmail.com>
2025-04-06 20:10:03 -0400

    allow http reques all origin

--- app-pedidos-ada/vite.config.js
+++ app-pedidos-ada/vite.config.js
@@ -5,13 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    // https: {
-    //   key: fs.readFileSync("./../certs/localhost+3-key.pem"),
-    //   cert: fs.readFileSync("./../certs/localhost+3.pem"),
-    // },
-    host: "0.0.0.0", // Para que sea accesible desde otros dispositivos
-    port: 5173, // O el puerto que prefieras
-  },
   plugins: [react(), tailwindcss()],
 });
